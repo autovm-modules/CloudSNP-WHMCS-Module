c32022d58d3f533efc14567df9054a92adfc085c
Salar Khosravi <salar.khosravi@gmail.com>
2023-11-05 17:25:19 +0330

    remove default language

--- modules/addons/cloudsnp/cloudsnp.php
+++ modules/addons/cloudsnp/cloudsnp.php
@@ -15,7 +15,6 @@ function cloudsnp_config()
         "fields" => array(
             "BackendUrl" => array ("FriendlyName" => "Backend Url", "Type" => "text", "Size" => "45", "Description" => "insert backedn Url with http", "Default" => "http://backend.autovm.online"),
             "ResellerToken" => array ("FriendlyName" => "Admin Token", "Type" => "text", "Size" => "25", "Description" => "you should insert your token here", "Default" => ""),
-            "DefLang" => array ("FriendlyName" => "Default Language", "Type" => "dropdown", "Options" => "en, fa, tr, fr, ru, du", "Description" => "Select Default language for your client and admin view panel", "Default" => "en", ),
         ));
         return $configarray;
 
@@ -43,16 +42,6 @@ function cloudsnp_clientarea($vars)
         }
     } else {
         echo('go to addons module and insert your Token');
-    }
-    
-    // get Default language from admin
-    if (array_key_exists('DefLang', $vars)) {
-        $DefLang = $vars['DefLang'];
-        if($DefLang == ''){
-            $DefLang = 'en';
-        }
-    } else {
-        $DefLang = 'en';
     }
     
     // Find action
